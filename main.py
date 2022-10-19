ae33fa03d62db526f7f80b27810c391d0761e253
Christopher Sund <christopher.sund@stratasys.com>
2022-10-19 16:30:23 -0700

    actually calculate the equal distance x/y/z ratios

--- main.py
+++ main.py
@@ -6,20 +6,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
-DATA_DIR = 'data-tahoe'
-RESIZE_FACTOR = 3.5
+DATA_DIR = 'data-astgtm'
 OUTPUT_RES = (5120, 3200)  # (5020, 2824) printable area
-BASE_LAYERS = 35
+PIXEL_SIZE = 38.25  # microns
+Z_LAYER_SIZE = 100  # microns
+DATA_RESOLTION_XY_deg = 1 / 3600  # 1 arc second
+DATA_RESOLUTION_XY = 30  # meters
+DATA_RESOLUTION_Z = 1  # meters
+PRINTABLE_AREA = (5020, 2824)
+PRINT_MARGIN_FACTOR = 0.85
+BASE_LAYERS = 50
+Z_HEIGHT_MULT = 1.0
 LAYER_REPEATS = 1
-LAYER_SKIPS = 2
-OUTPUT_DIR = 'out-tahoe'
+LAYER_SKIPS = 1
+OUTPUT_DIR = 'out-sf'
+NORTH_SOUTH_EXTENT = 0.3  # degrees latitude
 
-# center = (37.75657392417602, -122.44744937383727) # SF
-center = (39.089915551656375, -120.03122040686522)
-y_extent = 1
-x_extent = 16 * y_extent / 9
-region_TL = (center[1] + y_extent / 2, center[0] - x_extent / 2)
-region_BR = (center[1] - y_extent / 2, center[0] + x_extent / 2)
+center = (37.71580036603404, -122.39505208180861) # SF city
+# center = (37.809638612919, -122.36600653263841) # SF bay
+# center = (39.089915551656375, -120.03122040686522)  # LT
+EAST_WEST_EXTENT = 16 * NORTH_SOUTH_EXTENT / 9
+region_TL = (center[1] + NORTH_SOUTH_EXTENT / 2, center[0] - EAST_WEST_EXTENT / 2)
+region_BR = (center[1] - NORTH_SOUTH_EXTENT / 2, center[0] + EAST_WEST_EXTENT / 2)
 region_box = [region_TL, region_BR]
 print(region_box)
 
@@ -48,7 +56,7 @@ for fname in fnames:
         x_max = np.clip(x_max, 0, tif_shape[1])
         y_min = np.clip(y_min, 0, tif_shape[0])
         y_max = np.clip(y_max, 0, tif_shape[0])
-        z_data = np.array(alt_array[y_min:y_max, x_min:x_max])
+        z_data = np.array(alt_array[y_min:y_max, x_min:x_max]).astype(float)
         # print(int_box)
     except geotiff.BoundaryNotInTifError:
         print('bnite')
@@ -57,12 +65,17 @@ for fname in fnames:
         print('shape 0')
         continue
     
-    # z_data = np.flipud(np.transpose(z_data))
+    z_data = np.transpose(z_data)
     plt.imshow(z_data)
     plt.show()
-    continue
     base_size = z_data.shape[::-1]
-    resized_size = int(base_size[0] * RESIZE_FACTOR), int(base_size[1] * RESIZE_FACTOR)
+    base_size_meters = tuple([base_size[i] * DATA_RESOLUTION_XY for i in range(len(base_size))])
+    resize_factor = min(
+        [PRINTABLE_AREA[i] * PRINT_MARGIN_FACTOR / base_size[i] for i in range(len(PRINTABLE_AREA))]
+    )
+    z_resize_factor = resize_factor * (DATA_RESOLUTION_Z * Z_LAYER_SIZE) / (DATA_RESOLUTION_XY * PIXEL_SIZE)
+    z_data *= z_resize_factor * Z_HEIGHT_MULT
+    resized_size = int(base_size[0] * resize_factor), int(base_size[1] * resize_factor)
     print(resized_size)
     fill_layer = Image.new('L', resized_size, 'white')
     full_fill_img = Image.new('L', OUTPUT_RES)
